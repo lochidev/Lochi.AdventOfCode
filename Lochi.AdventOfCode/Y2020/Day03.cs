f016b592c33f76fe65af013921fd1e8648e3d5fa
lochidev <starcrasher54@gmail.com>
2021-08-18 10:37:54 +0530

    Another garabge algorithm, day 3 complete - 5.283 us

--- Lochi.AdventOfCode/Y2020/Day03.cs
+++ Lochi.AdventOfCode/Y2020/Day03.cs
@@ -7,27 +7,87 @@ namespace Lochi.AdventOfCode.Y2020
         public Solution Solve(ReadOnlySpan<char> input)
         {
             int part1 = 0;
-            int part2 = 0;
+            int r1d1 = 0;
+            int r1d2 = 0;
+            int r5d1 = 0;
+            int r7d1 = 0;
+            int r1d1Position = 0;
+            int r1d2Position = 1;
+            int r5d1Position = 0;
+            int r7d1Position = 0;
             int linePosition = 0;
-            foreach (var line in input.SplitLines())
+            bool skipped = false;
+            bool start = true;
+            foreach (ReadOnlySpan<char> line in input.SplitLines())
             {
                 //...#.....#.......##......#.....
                 //...#..................#........
                 //....##....#.......#............
                 //.........#.......#.......#.....
-                //...#....##...#...#...#.#..#....
+                //..#..............#.........#..#
+                //.....#.........#....#....#....#
+                //....##..........#.#.##.........
                 int maxPosition = (line.Length - 1);
-                if (linePosition > maxPosition)
+                //when 1 right 1 down
+                if (!start)
                 {
-                    linePosition -= maxPosition + 1;
-                }
-                if (line[linePosition] == '#')
-                {
-                    part1++;
+                    //1 2 3 4 5 6
+                    if (r1d1Position > maxPosition)
+                    {
+                        r1d1Position -= maxPosition + 1;
+                    }
+                    if (line[r1d1Position] == '#')
+                    {
+                        r1d1++;
+                    }
+                    //when 1 right 2 down
+                    if (skipped)
+                    {
+                        if (r1d2Position > maxPosition)
+                        {
+                            r1d2Position -= maxPosition + 1;
+                        }
+                        if (line[r1d2Position] == '#')
+                        {
+                            r1d2++;
+                        }
+                        r1d2Position++;
+                    }
+                    skipped = !skipped;
+                    //when 5 right 1 down
+                    if (r5d1Position > maxPosition)
+                    {
+                        r5d1Position -= maxPosition + 1;
+                    }
+                    if (line[r5d1Position] == '#')
+                    {
+                        r5d1++;
+                    }
+                    //when 7 right 1 down
+                    if (r7d1Position > maxPosition)
+                    {
+                        r7d1Position -= maxPosition + 1;
+                    }
+                    if (line[r7d1Position] == '#')
+                    {
+                        r7d1++;
+                    }
+                    if (linePosition > maxPosition)
+                    {
+                        linePosition -= maxPosition + 1;
+                    }
+                    if (line[linePosition] == '#')
+                    {
+                        part1++;
+                    }
                 }
                 linePosition += 3;
+                r1d1Position++;
+                r5d1Position += 5;
+                r7d1Position += 7;
+                start = false;
             }
-            return new Solution(part1, part2);
+            return new Solution(part1.ToString(), (r1d1 * r1d2 * r5d1 * r7d1 * part1).ToString());
         }
     }
 }
