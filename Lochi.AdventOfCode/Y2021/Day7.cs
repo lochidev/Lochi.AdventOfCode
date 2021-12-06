2dc8256ca1f60cbc89dcd3cc1c72b6621438f3c0
lochi <72388533+lochidev@users.noreply.github.com>
2021-12-07 01:39:56 +0530

    day7

---
+++ Lochi.AdventOfCode/Y2021/Day7.cs
@@ -0,0 +1,44 @@
+﻿using Lochi.AdventOfCode.Helpers;
+
+namespace Lochi.AdventOfCode.Y2021;
+
+public class Day7 : ISolver
+{
+    public Solution Solve(ReadOnlySpan<char> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Solution Solve(string input)
+    {
+        var positions = input.Split(',').Select(int.Parse).ToArray();
+        int leastSum = 0;
+        for (int i = 0; i < positions.Length; i++)
+        {
+            int sum = positions.Sum(position => Math.Abs(positions[i] - position));
+            if (leastSum != 0 && sum >= leastSum) continue;
+            leastSum = sum;
+        }
+
+        var part1 = leastSum;
+        //part2
+        leastSum = 0;
+        for (int i = 0; i < positions.Length; i++)
+        {
+            int sum = positions.Sum(position => CalculateSum(Math.Abs(positions[i] - position)));
+            if (leastSum != 0 && sum >= leastSum) continue;
+            leastSum = sum;
+        }
+        return new Solution(part1, leastSum);
+    }
+
+    private static int CalculateSum(int diff)
+    {
+        int sum = 0;
+        for (int i = 1; i < diff + 1; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
